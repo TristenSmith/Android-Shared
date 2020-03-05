494a768f29b731605701ebe38cf08c322e52d39d
Tristen Smith <tristen.smith500@gmail.com>
2020-03-05 14:21:41 -0700

    Added RandomNumberWidget

---
+++ RandomNumberWidget/app/src/main/java/ca/tristensmith/randomnumberwidget/RandomWidgetProvider.java
@@ -0,0 +1,59 @@
+package ca.tristensmith.randomnumberwidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import java.util.Random;
+
+public class RandomWidgetProvider extends AppWidgetProvider
+{
+    private static final String TAG = "RandomNumberWidget";
+    RemoteViews views;
+
+    public static int currentColor = 1;
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+    {
+        ComponentName thisWidget = new ComponentName(context, RandomWidgetProvider.class);
+        int[] widgetIdArray = appWidgetManager.getAppWidgetIds(thisWidget);
+        for (int widgetID: widgetIdArray)
+        {
+            int number = new Random().nextInt(100);
+            switch (currentColor)
+            {
+                case 1:
+                {
+                    views = new RemoteViews(context.getPackageName(), R.layout.widget_layout_blue);
+                    currentColor = 2;
+                    break;
+                }
+                case 2:
+                {
+                    views = new RemoteViews(context.getPackageName(), R.layout.widget_layout_red);
+                    currentColor = 3;
+                    break;
+                }
+                case 3:
+                {
+                    views = new RemoteViews(context.getPackageName(), R.layout.widget_layout_green);
+                    currentColor = 1;
+                    break;
+                }
+            }
+            Intent intent = new Intent(context, RandomWidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            views.setOnClickPendingIntent(R.id.textview_random_number, pendingIntent);
+            views.setTextViewText(R.id.textview_random_number, String.valueOf(number));
+            appWidgetManager.updateAppWidget(widgetID, views);
+        }//eo/foreach
+    }//eo\onUpdate
+}
